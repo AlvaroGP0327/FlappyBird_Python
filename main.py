840a6ad8dd9a482ad48fc50df13ff31e2f70531e
AlvaroGP0327 <customerm552@gmail.com>
2024-07-20 18:52:08 -0400

    Se dibuja el player Bird, se implementa la animacion para caida,salto y media altura

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame, sys, os
 from random import randint, choice
+import random
+
 
 pygame.init()
 size = (576,1024) 
@@ -60,11 +62,37 @@ class Pipe(pygame.sprite.Sprite):
         self.movement_pipe()
         self.destroy_pipes()
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/redbird-midflap.png').convert_alpha()
+        self.image = pygame.transform.scale2x(self.image)
+        self.rect = self.image.get_rect(topleft=(50,512))
+        
+        #bird object initiate with force gravity action.
+        self.gravity = 1
+        self.fly_bird = 1
+    
+    def gravity_force_for_bird(self):
+        self.rect.y += self.gravity
+        self.image = pygame.image.load('assets/redbird-upflap.png').convert_alpha()
+        self.image = pygame.transform.scale2x(self.image)
+    
+    def jump_bird(self):
+        self.rect.y -= self.fly_bird
+        self.image = pygame.image.load('assets/redbird-downflap.png').convert_alpha()    
+        self.image = pygame.transform.scale2x(self.image)
+        
+    def update(self):
+        self.gravity_force_for_bird()
+        #self.jump_bird()
+
 #Instances
 sky = Sky()
 ground = Ground()
 ground_2 = Ground()
 ground_2.initial_position()
+bird = Bird()
 
 #Sprite Groups
 sky_group = pygame.sprite.GroupSingle()
@@ -74,11 +102,14 @@ ground_group = pygame.sprite.Group()
 ground_group.add(ground)
 ground_group.add(ground_2)
 
+player_group = pygame.sprite.GroupSingle()
+player_group.add(bird)
+
 obstacle_group = pygame.sprite.Group()
 
 #Timer for create random pipes obstalces.
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer,2800)
+pygame.time.set_timer(obstacle_timer,2000)
 
 pipe_gap = -300
 
@@ -92,6 +123,7 @@ while True:
             #set position before instantiate pipes
             #random position for bottom pipe
             topleft_pipe_position = choice([400,500,600,750]) #topleft
+            print(topleft_pipe_position)
             pipe = Pipe(topleft_pipe_position)
             #get the y position relative to bottom pipe position
             top_pipe_position = pipe.rect.topleft[1]
@@ -100,11 +132,16 @@ while True:
             obstacle_group.add(pipe)
             obstacle_group.add(pipe_2)
             
+    #draw and update all scenario
     ground_group.update()
     obstacle_group.update()
     sky_group.draw(screen)
     obstacle_group.draw(screen)
     ground_group.draw(screen)
+    
+    #draw and update player
+    player_group.draw(screen)
+    player_group.update()
     
     pygame.display.update()
     
